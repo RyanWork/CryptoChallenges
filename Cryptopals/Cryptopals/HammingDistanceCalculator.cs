f2c28bc968fb091159df7a2ba787138c73cbfe94
rha <rha@ciena.com>
2017-07-18 13:45:32 -0400

    Changed the return type to float

--- Cryptopals/Cryptopals/HammingDistanceCalculator.cs
+++ Cryptopals/Cryptopals/HammingDistanceCalculator.cs
@@ -14,7 +14,7 @@ namespace Cryptopals
     /// <param name="originalText">First text input</param>
     /// <param name="newText">The text to change to</param>
     /// <returns>An integer calculation representing the hamming distance</returns>
-    public int CalculateDistance(byte[] originalText, byte[] newText)
+    public float CalculateDistance(byte[] originalText, byte[] newText)
     {
       if (originalText.Length != newText.Length)
         throw new Exceptions.UnequalLengthException();
